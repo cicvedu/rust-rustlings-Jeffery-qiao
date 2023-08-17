13e0614656c934029c9f1ff47e8811e32ca8a71a
Jeffery-qiao <1648496887@qq.com>
2023-08-17 22:05:54 +0800

    anytging

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,7 +16,7 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 pub struct ReportCard {
     pub grade: f32,
@@ -26,8 +26,38 @@ pub struct ReportCard {
 
 impl ReportCard {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, self.format_grade()
+        )
+    }
+
+    fn format_grade(&self) -> String {
+        if self.grade >= 4.0 {
+            "A+".to_string()
+        } else if self.grade >= 3.7 {
+            "A".to_string()
+        } else if self.grade >= 3.3 {
+            "A-".to_string()
+        } else if self.grade >= 3.0 {
+            "B+".to_string()
+        } else if self.grade >= 2.7 {
+            "B".to_string()
+        } else if self.grade >= 2.3 {
+            "B-".to_string()
+        } else if self.grade >= 2.0 {
+            "C+".to_string()
+        } else if self.grade >= 1.7 {
+            "C".to_string()
+        } else if self.grade >= 1.3 {
+            "C-".to_string()
+        } else if self.grade >= 1.0 {
+            "D+".to_string()
+        } else if self.grade >= 0.7 {
+            "D".to_string()
+        } else {
+            "F".to_string()
+        }
     }
 }
 
@@ -44,15 +74,14 @@ mod tests {
         };
         assert_eq!(
             report_card.print(),
-            "Tom Wriggle (12) - achieved a grade of 2.1"
+            "Tom Wriggle (12) - achieved a grade of C+"
         );
     }
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: 4.5,
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
